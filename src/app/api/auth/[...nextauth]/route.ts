c118a0e7ac46b648a6475db28e3478c5bd652be7
Arjen Bloemsma <arjen@bloemium.com>
2023-06-02 21:24:14 +0200

    github auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,6 @@
+import { authOptions } from '@/lib/auth'
 import NextAuth from 'next-auth/next'
-import GithubProvider from 'next-auth/providers/github'
 
-const handler = NextAuth({
-   providers: [
-      GithubProvider({
-         clientId: process.env.GITHUB_CLIENT_ID,
-         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      })
-   ],
-})
-
-export default NextAuth(authOptions)
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
