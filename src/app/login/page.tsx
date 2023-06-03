25751c2997bc807f6f06a0b2b103a5ea392229e6
Arjen Bloemsma <arjen@bloemium.com>
2023-06-03 19:34:59 +0200

    added toast

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,24 +2,30 @@
 
 import { signIn } from 'next-auth/react'
 import { Button } from '@/components/ui/button'
-
-const loginWithGithub = async () => {
-  try {
-    await signIn('github')
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-const loginWithAuth0 = async () => {
-  try {
-    await signIn('auth0')
-  } catch (error) {
-    console.error(error)
-  }
-}
+import toast from 'react-hot-toast'
+import { useState } from 'react'
 
 export default function LoginPage() {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const loginWith = async (providerName: string, errorMessage: string) => {
+    setIsLoading(true)
+    try {
+      await signIn(providerName)
+    } catch (error) {
+      setIsLoading(false)
+      toast.error(errorMessage)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const loginWithGithub = () =>
+    loginWith('github', 'Error signing in with Github')
+
+  const loginWithAuth0 = () =>
+    loginWith('auth0', 'Error signing in with Auth0')
+
   return (
     <>
       <div>Login Page</div>
