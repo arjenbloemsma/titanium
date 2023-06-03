b12e073f140ec56b4e11ef4e7ab530dc78356ba5
Arjen Bloemsma <arjen@bloemium.com>
2023-06-03 16:22:52 +0200

    Added auth0

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,11 +11,20 @@ const loginWithGithub = async () => {
   }
 }
 
+const loginWithAuth0 = async () => {
+  try {
+    await signIn('auth0')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 export default function LoginPage() {
   return (
     <>
       <div>Login Page</div>
-      <Button onClick={loginWithGithub}>Sign In</Button>
+      <Button onClick={loginWithGithub}>Sign in using Github</Button>
+      <Button onClick={loginWithAuth0}>Sign in using Auth0</Button>
     </>
   );
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github"
+import Auth0Provider from "next-auth/providers/auth0"
 
 const getGithubCredentials = () => {
   const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env
@@ -15,6 +16,24 @@ const getGithubCredentials = () => {
   }
 }
 
+const getAuth0Credentials = () => {
+  const { AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_ISSUER } = process.env
+  if (!AUTH0_CLIENT_ID || !AUTH0_CLIENT_ID.length) {
+    throw new Error('AUTH0_CLIENT_ID is not set')
+  }
+  if (!AUTH0_CLIENT_SECRET || !AUTH0_CLIENT_SECRET.length) {
+    throw new Error('AUTH0_CLIENT_SECRET is not set')
+  }
+  if (!AUTH0_ISSUER || !AUTH0_ISSUER.length) {
+    throw new Error('AUTH0_ISSUER is not set')
+  }
+  return {
+    clientId: AUTH0_CLIENT_ID,
+    clientSecret: AUTH0_CLIENT_SECRET,
+    issuer: AUTH0_ISSUER,
+  }
+}
+
 export const authOptions: NextAuthOptions = {
   pages: {
     signIn: 'login',
@@ -22,6 +41,9 @@ export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       ...getGithubCredentials(),
-    })
+    }),
+    Auth0Provider({
+      ...getAuth0Credentials(),
+    }),
   ],
 }
