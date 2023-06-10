6e8bb6c133ebf3aa9d1eeb2bf1c1a0de2286d7e1
Arjen Bloemsma <arjen@bloemium.com>
2023-06-10 05:48:49 +0200

    Logged in user email is shown

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,16 +1,20 @@
 'use client'
 
+import { SessionProvider } from 'next-auth/react'
 import { FC, ReactNode } from 'react'
 import { Toaster } from 'react-hot-toast'
 
 interface ProviderProps {
    children: ReactNode
+   session: unknown
 }
 
-const Providers: FC<ProviderProps> = ({ children }) => (
+const Providers: FC<ProviderProps> = ({ children, session }) => (
    <>
-      <Toaster position="top-center" reverseOrder={false} />
-      {children}
+      <SessionProvider session={session}>
+         <Toaster position="top-center" reverseOrder={false} />
+         {children}
+      </SessionProvider>
    </>
 )
 
